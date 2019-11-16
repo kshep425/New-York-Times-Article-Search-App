f5287d10127db40f0e73f825af820a7eb4d41f38
kshep425 <9432996+kshep425@users.noreply.github.com>
2019-11-16 13:59:52 -0500

    Added the Results to page

--- assets/js/nyt_script.js
+++ assets/js/nyt_script.js
@@ -1,6 +1,6 @@
 console.log("New York Times Article Search Started")
 let search_term, num_records, start_year, end_year;
-var resultsDiv = $("results_div")
+var resultsDiv = $("#results_div")
 
 $("#search_button").on("click", function() {
     execute_query();
@@ -51,21 +51,36 @@ function execute_query(){
     })
 }
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+// <div class="card" style="width: 18rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <a href="#" class="card-link">Card link</a>
+//   </div>
+// </div>
 
 function add_results(results){
     console.log(results)
-    let card, card_body, article_title, article_author, article_abstract,
-    card = $("<div>").addClass("card");
-    card_body = $("<div>").addClass("card-body");
-    article_title = $("<h5>").addClass("card-title").text()
+    let card, card_body, article_title, article_author, article_abstract, article_url;
+
+    results.forEach(result => {
+        console.log(result)
+        card = $("<div>").addClass("card");
+        card_body = $("<div>").addClass("card-body");
+        article_title = $("<h5>").addClass("card-title").text(result.headline.main);
+        article_author = $("<h6>").addClass("card-subtitle mb2 text-muted").text(result.byline.original);
+        article_abstract = $("<p>").addClass("card-text").text(result.abstract);
+        article_url = $("<a>").addClass("card-link");
+        article_url.attr("href", result.web_url);
+        article_url.text("Link to Article");
+
+        resultsDiv.append(card);
+        card.append(card_body);
+        card_body.append(article_title);
+        card_body.append(article_author);
+        card_body.append(article_abstract);
+        card_body.append(article_url);
+    })
 
 }
